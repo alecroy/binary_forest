a29a27b81e831509a555254e4c538d74487bfd14
Alec Hollingsworth <alec@mailc.net>
2015-11-10 22:22:12 -0500

    Reformat whitespace, rename variables.
    Untabify!  Bad Sublime!

--- binary_forest.erl
+++ binary_forest.erl
@@ -4,33 +4,17 @@
 -include_lib("eunit/include/eunit.hrl").
 
 
+
 create() -> create([]).
 create(List) ->
-	Size = length(List),
-	#forest{size=Size, trees=trees(Size, List, [])}.
-
-trees(_Size, [], Trees) -> Trees;
-trees(Size, List, Trees) ->
-	TreeSize = highest_power_of_2(Size),
-	{Rest, Values} = lists:split(Size - TreeSize, List), % take values off end
-	Tree = #tree{size=TreeSize, values=Values},
-	trees(Size - TreeSize, Rest, [Tree | Trees]).
-
-highest_power_of_2(N) -> trunc(math:pow(2, trunc(math:log2(N)))).
+    Size = length(List),
+    #forest{size=Size, trees=trees(Size, List, [])}.
 
 
 cons(Element, Forest) ->
-	NewSize = Forest#forest.size + 1,
-	Tree = #tree{size=1, values=[Element]},
-	#forest{size=NewSize, trees=merge(Tree, Forest#forest.trees)}.
-
-merge(Tree, []) -> [Tree];
-merge(Tree, [T | Trees]) when Tree#tree.size < T#tree.size ->
-	[Tree] ++ [T | Trees];
-merge(Tree, [T | Trees]) ->
-	Size = 2 * Tree#tree.size,
-	Values = Tree#tree.values ++ T#tree.values,
-	merge(#tree{size=Size, values=Values}, Trees).
+    NewSize = Forest#forest.size + 1,
+    Tree = #tree{size=1, values=[Element]},
+    #forest{size=NewSize, trees=merge(Tree, Forest#forest.trees)}.
 
 
 is_empty(Forest) -> Forest#forest.size == 0.
@@ -41,45 +25,76 @@ head(#forest{trees=[#tree{values=[Value | _Values]} | _Trees]}) -> Value.
 
 tail(Forest = #forest{trees=[]}) -> Forest;
 tail(#forest{size=Size, trees=[#tree{values=[_Value | Values]} | Trees]}) ->
-	SubForest = create(Values),
-	#forest{size=Size - 1, trees=SubForest#forest.trees ++ Trees}.
+    SubForest = create(Values),
+    #forest{size=Size - 1, trees=SubForest#forest.trees ++ Trees}.
 
 
 
+%%% helpers
+
+highest_power_of_2(N) -> trunc(math:pow(2, trunc(math:log2(N)))).
+
+
+%% Split a List of length Size into trees each of size 2^k.
+trees(_Length, [], Trees) -> Trees;
+trees(Length, List, Trees) ->
+    TreeSize = highest_power_of_2(Length),
+    {Rest, Values} = lists:split(Length - TreeSize, List), % take values off end
+    Tree = #tree{size=TreeSize, values=Values},
+    trees(Length - TreeSize, Rest, [Tree | Trees]).
+
+
+%% Merge a Tree with other Trees of size 2^k (like adding 1 to a binary number)
+merge(Tree, []) -> [Tree];
+merge(Tree, [T | Trees]) when Tree#tree.size < T#tree.size ->
+    [Tree] ++ [T | Trees];
+merge(Tree, [T | Trees]) ->
+    Size = 2 * Tree#tree.size,
+    Values = Tree#tree.values ++ T#tree.values,
+    merge(#tree{size=Size, values=Values}, Trees).
+
+
+
+%%% tests
 
 create_test() ->
-	#forest{size=0, trees=[]} = create(),
-	#forest{size=1, trees=[#tree{size=1, values=[1]}]} = create([1]),
-	#forest{size=2, trees=[#tree{size=2, values=[1, 2]}]} = create([1, 2]),
-	#forest{size=6, trees=[#tree{size=2, values=[1, 2]},
-						   #tree{size=4, values=[3, 4, 5, 6]}
-						   ]} = create(lists:seq(1, 6)),
-	ok.
+    #forest{size=0, trees=[]} = create(),
+    #forest{size=1, trees=[#tree{size=1, values=[1]}]} = create([1]),
+    #forest{size=2, trees=[#tree{size=2, values=[1, 2]}]} = create([1, 2]),
+    #forest{size=6, trees=[#tree{size=2, values=[1, 2]},
+                           #tree{size=4, values=[3, 4, 5, 6]}
+                           ]} = create(lists:seq(1, 6)),
+    ok.
+
 
 highest_power_of_2_test() ->
-	16 = highest_power_of_2(17),
-	16 = highest_power_of_2(16),
-	8 = highest_power_of_2(15),
-	ok.
+    16 = highest_power_of_2(17),
+    16 = highest_power_of_2(16),
+    8 = highest_power_of_2(15),
+    ok.
+
 
 cons_test() ->
-	#forest{size=1, trees=[#tree{size=1, values=[1]}]} = cons(1, create()),
-	Three = #forest{size=3, trees=[#tree{size=1, values=[1]},
-	                               #tree{size=2, values=[2, 3]}]},
-	Three = cons(1, cons(2, cons(3, create()))),
-	ok.
+    #forest{size=1, trees=[#tree{size=1, values=[1]}]} = cons(1, create()),
+    Three = #forest{size=3, trees=[#tree{size=1, values=[1]},
+                                   #tree{size=2, values=[2, 3]}]},
+    Three = cons(1, cons(2, cons(3, create()))),
+    ok.
+
 
 is_empty_test() ->
-	true = is_empty(create()),
-	false = is_empty(create([1])),
-	ok.
+    true = is_empty(create()),
+    false = is_empty(create([1])),
+    ok.
+
 
 tail_test() ->
-	#forest{size=0, trees=[]} = tail(create([1])),
-	#forest{size=1, trees=[#tree{size=1, values=[2]}]} = tail(create([1, 2])),
-	ok.
+    #forest{size=0, trees=[]} = tail(create([1])),
+    #forest{size=1, trees=[#tree{size=1, values=[2]}]} = tail(create([1, 2])),
+    ok.
+
 
 head_test() ->
-	1 = head(create([1])),
-	3 = head(tail(tail(create([1, 2, 3])))),
-	ok.
+    1 = head(create([1])),
+    3 = head(tail(tail(create([1, 2, 3])))),
+    ok.
